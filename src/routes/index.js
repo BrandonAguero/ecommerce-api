15f422d041991364729a36e5b4cd9c6f9ca517da
Brandon <name.brandon.aguero@gmail.com>
2023-08-18 09:15:28 -0500

    Agregue el model purchase, con sus controladores, ruta y relaciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const { verifyJwt } = require("../utils/verifyJwt");
+
+const router = express.Router();
+
 const routerUser = require("./user.router");
 const routerCategory = require("./category.router");
 const routerProduct = require("./product.router");
 const routerCart = require("./cart.router");
-const { verifyJwt } = require("../utils/verifyJwt");
-const router = express.Router();
+const routerPurchase = require("./purchase.router.js");
 
 // colocar las rutas aquí
 router.use("/users", routerUser);
 router.use("/categories", routerCategory);
 router.use("/products", routerProduct);
-router.use("/cart", verifyJwt, routerCart);
+router.use("/cart", verifyJwt, routerCart); //🔏🔏🔏🔏🔏🔏🔏🔏
+router.use("/purchase", verifyJwt, routerPurchase); //🔏🔏🔏🔏🔏🔏🔏🔏
 
 module.exports = router;
